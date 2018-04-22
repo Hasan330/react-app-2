6822ef5b0c24f17546c5221aa70a783c00940d61
hasan330 <hasan.saleh.khader@gmail.com>
2018-04-22 09:17:13 +0300

    More on reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 import ActiveBook from './reducer_active_book';
 
+//Any key to the object we provide to combineReducer ends up as a key on our global state 
 const rootReducer = combineReducers({
   books: BooksReducer,
   activeBook: ActiveBook,
